e07664edf6efc2b6883ca03da23524190a09ee5c
Rahul Madhavan <rahul.maddy@gmail.com>
2016-11-28 04:13:55 +0530

    added navbar for logout

---
+++ static/javascripts/layout/controllers/navbar.controller.js
@@ -0,0 +1,35 @@
+/**
+ * Created by bhive on 28-11-2016.
+ */
+
+/**
+* NavbarController
+* @namespace cmath.layout.controllers
+*/
+(function () {
+  'use strict';
+
+  angular
+    .module('thinkster.layout.controllers')
+    .controller('NavbarController', NavbarController);
+
+  NavbarController.$inject = ['$scope', 'Authentication'];
+
+  /**
+  * @namespace NavbarController
+  */
+  function NavbarController($scope, Authentication) {
+    var vm = this;
+
+    vm.logout = logout;
+
+    /**
+    * @name logout
+    * @desc Log the user out
+    * @memberOf cmath.layout.controllers.NavbarController
+    */
+    function logout() {
+      Authentication.logout();
+    }
+  }
+})();
