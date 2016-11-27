72ef48ef87469b16b93ec59ffd65c4187316c90e
Rahul Madhavan <rahul.maddy@gmail.com>
2016-11-28 01:49:58 +0530

    angular dependencies flow all the way, so all have to be defined i suppose till the top level

---
+++ static/javascripts/cmath.js
@@ -0,0 +1,16 @@
+/**
+ * Created by bhive on 28-11-2016.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('cmath', [
+      'routes',
+      'authentication'
+    ]);
+
+  angular
+    .module('routes', ['ngRoute']);
+})();
+
