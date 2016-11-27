283e760d25b642e0f28c178f9e25ef94c7db78e9
Rahul Madhavan <rahul.maddy@gmail.com>
2016-11-28 01:39:44 +0530

    register controller in angular

---
+++ static/javascripts/authentication/controllers/register.controller.js
@@ -0,0 +1,35 @@
+/**
+ * Created by bhive on 28-11-2016.
+ */
+
+/**
+* Register controller
+* @namespace authentication.controllers
+*/
+(function () {
+  'use strict';
+
+  angular
+    .module('authentication.controllers')
+    .controller('RegisterController', RegisterController);
+
+  RegisterController.$inject = ['$location', '$scope', 'Authentication'];
+
+  /**
+  * @namespace RegisterController
+  */
+  function RegisterController($location, $scope, Authentication) {
+    var vm = this;
+
+    vm.register = register;
+
+    /**
+    * @name register
+    * @desc Register a new user
+    * @memberOf thinkster.authentication.controllers.RegisterController
+    */
+    function register() {
+      Authentication.register(vm.email, vm.password, vm.username);
+    }
+  }
+})();
