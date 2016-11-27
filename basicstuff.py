5da6a559e48ff6f61e6a9ba7133fb5ec3f56cc0b
Rahul Madhavan <rahul.maddy@gmail.com>
2016-11-28 00:23:00 +0530

    authentication model for user

--- basicstuff.py
+++ basicstuff.py
@@ -1,4 +1,11 @@
 
 # Install everything on this site
 # https://github.com/p10rahulm/thinkster-django-angular-boilerplate
+ 
+ # activate djangularfifi environment
+activate djangularfifi
+ # start the server by typing
+python manage.py runserver
+
+python manage.py startapp authentication
 
