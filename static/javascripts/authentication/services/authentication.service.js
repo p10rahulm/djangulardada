10865995daf4befd82340e9a1995011f6c0f42e1
Rahul Madhavan <rahul.maddy@gmail.com>
2016-11-28 01:30:39 +0530

    added some angular auth services

---
+++ static/javascripts/authentication/services/authentication.service.js
@@ -0,0 +1,46 @@
+/**
+ * Created by bhive on 28-11-2016.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('authentication.services')
+    .factory('Authentication', Authentication);
+
+  Authentication.$inject = ['$cookies', '$http'];
+
+  /**
+  * @namespace Authentication
+  * @returns {Factory}
+  */
+  function Authentication($cookies, $http) {
+    /**
+    * @name Authentication
+    * @desc The Factory to be returned
+    */
+    var Authentication = {
+      register: register
+    };
+
+    return Authentication;
+
+    ////////////////////
+
+    /**
+    * @name register
+    * @desc Try to register a new user
+    * @param {string} username The username entered by the user
+    * @param {string} password The password entered by the user
+    * @param {string} email The email entered by the user
+    * @returns {Promise}
+    */
+    function register(email, password, username) {
+      return $http.post('/api/v1/accounts/', {
+        username: username,
+        password: password,
+        email: email
+      });
+    }
+  }
+})();
